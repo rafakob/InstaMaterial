1973dd4ac632c575dcaaf57c97de30d1ce5b799f
Rafal <rafal@tsh.io>
2015-12-03 13:04:52 +0100

    Part01 finished

--- app/src/main/java/io/tsh/rafal/instamaterial/MainActivity.java
+++ app/src/main/java/io/tsh/rafal/instamaterial/MainActivity.java
@@ -1,13 +1,122 @@
 package io.tsh.rafal.instamaterial;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int ANIM_DURATION_FAB = 400;
+    private static final int ANIM_DURATION_TOOLBAR = 300;
+    private static final int ANIM_DURATION_FEED = 400;
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.rvFeed) RecyclerView rvFeed;
+    @Bind(R.id.fab) FloatingActionButton btnCreate;
+    @Bind(R.id.ivLogo) ImageView ivLogo;
+    MenuItem inboxMenuItem;
+    boolean pendingIntroAnimation = false;
+    private FeedAdapter feedAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (savedInstanceState == null)
+            pendingIntroAnimation = true;
+
+        setupToolbar();
+        setupFeed();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+    }
+
+    private void setupFeed() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvFeed.setLayoutManager(linearLayoutManager);
+        feedAdapter = new FeedAdapter(this);
+        rvFeed.setAdapter(feedAdapter);
+        feedAdapter.updateItems();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        inboxMenuItem = menu.findItem(R.id.action_inbox).setActionView(R.layout.menu_item_view);
+
+        if (pendingIntroAnimation) {
+            pendingIntroAnimation = false;
+            startIntroAnimation();
+        }
+        return true;
+    }
+
+    private void startIntroAnimation() {
+        btnCreate.setTranslationY(2 * getResources().getDimensionPixelOffset(R.dimen.btn_fab_size));
+
+        int actionbarSize = Utils.dpToPx(56);
+        toolbar.setTranslationY(-actionbarSize);
+        ivLogo.setTranslationY(-actionbarSize);
+        inboxMenuItem.getActionView().setTranslationY(-actionbarSize);
+        rvFeed.setTranslationY(100);
+        rvFeed.setAlpha(0.0f);
+
+
+        toolbar.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(300);
+
+        ivLogo.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(400);
+
+        inboxMenuItem.getActionView().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(500)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        startContentAnimation();
+                    }
+                })
+                .start();
+    }
+
+    private void startContentAnimation() {
+        btnCreate.animate()
+                .translationY(0)
+                .setInterpolator(new OvershootInterpolator(1.f))
+                .setStartDelay(100 + ANIM_DURATION_FEED)
+                .setDuration(ANIM_DURATION_FAB)
+                .start();
+
+        rvFeed.animate()
+                .translationY(0)
+                .alpha(1.0f)
+                .setInterpolator(new DecelerateInterpolator())
+                .setStartDelay(100)
+                .setDuration(ANIM_DURATION_FEED)
+                .start();
     }
 }
